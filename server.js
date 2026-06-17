38b637bb5b374d6b53aa3ffa55e2f2df0b65fc39
manishyadav-hash <manish.yadav@jarvis.consulting>
2026-06-17 21:59:25 +0530

    database url fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
+console.log(process.env.DATABASE_URL);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
